c9f17fd65372158f84a2fb91198543c6142b856d
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-01 14:18:46 +0530

    Added body-parser

--- src/Server.js
+++ src/Server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 dotenv.config();
 
 const PORT = process.env.PORT 
 
 const setupServer = async() =>{
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, () =>{
     console.log(`Server started listening on ${PORT}`);
-})
+});
 }
 
 setupServer();
