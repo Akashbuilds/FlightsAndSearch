617a9fab1cda8aaaac09fdcda46ffa72803435d8
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-03 02:17:45 +0530

    Added the city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,10 +3,13 @@ const { City } = require("../models/index");
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City.create({ 
+        name
+     });
       return city;
     } catch (error) {
-      throw { error };
+      console.log("Something Went Wrong in the Repository Layer");
+      throw {error};
     }
   }
   async deleteCity(cityId) {
@@ -16,8 +19,35 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
-      throw { error };
+        console.log("Something Went Wrong in the Repository Layer");
+        throw { error };
+    }
+  }
+  async updateCity(cityId,data) {
+    try {
+        const city = await City.update(data,{
+            where : {
+                id : cityId
+            } 
+        }
+            );
+            return city;
+    } catch (error) {
+        console.log("Something Went Wrong in the Repository Layer");
+        throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+        const city = await City.findbyPk(cityId);
+        return city;
+    } catch (error) {
+        console.log("Something Went Wrong in the Repository Layer");
+        throw { error };
+        
     }
   }
 }
