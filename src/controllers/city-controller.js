f3ce45a9889ef97ee22ce48f5f5d55f2db349227
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-05 19:41:19 +0530

    Added more routers to city api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -95,5 +95,5 @@ module.exports = {
     create,
     destroy,
     update,
-    get
+    get,
 }
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,15 +1,15 @@
-const  { City }  = require("../models/index");
+const { City } = require("../models/index");
 
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ 
-        name
-     });
+      const city = await City.create({
+        name,
+      });
       return city;
     } catch (error) {
       console.log("Something Went Wrong in the Repository Layer");
-      throw {error};
+      throw { error };
     }
   }
   async deleteCity(cityId) {
@@ -21,33 +21,31 @@ class CityRepository {
       });
       return true;
     } catch (error) {
-        console.log("Something Went Wrong in the Repository Layer");
-        throw { error };
+      console.log("Something Went Wrong in the Repository Layer");
+      throw { error };
     }
   }
-  async updateCity(cityId,data) {
+  async updateCity(cityId, data) {
     try {
-        const city = await City.update(data,{
-            where : {
-                id : cityId
-            } 
-        }
-            );
-            return city;
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
     } catch (error) {
-        console.log("Something Went Wrong in the Repository Layer");
-        throw { error };
+      console.log("Something Went Wrong in the Repository Layer");
+      throw { error };
     }
   }
 
   async getCity(cityId) {
     try {
-        const city = await City.findbyPk(cityId);
-        return city;
+      const city = await City.findByPk(cityId);
+      return city;
     } catch (error) {
-        console.log("Something Went Wrong in the Repository Layer");
-        throw { error };
-        
+      console.log("Something Went Wrong in the Repository Layer");
+      throw { error };
     }
   }
 }
